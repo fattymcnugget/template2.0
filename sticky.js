3fc07a98beddda7b34ce9a9b4f964a157e8b21de
fattymcnugget <33332184+fattymcnugget@users.noreply.github.com>
2018-06-04 20:32:27 +0200

    Initial commit

---
+++ sticky.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(document).scrollTop() > 10) {
+            $('#nav').addClass('shrink');
+        }
+        else {
+            $('#nav').removeClass('shrink');
+        }
+    });
+});
